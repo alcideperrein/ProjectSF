3caa5bbad0d0b797fe971471c08d06079ea0fb53
matteobelin <matteobelin85@gmail.com>
2023-04-05 20:01:08 +0200

    Update main.c

--- main.c
+++ main.c
@@ -32,17 +32,14 @@ void haut(Coordonees* point)
 int main(int argc, char **argv) {
     Coordonees point;
     SDL_Window* pwindow;
+    SDL_Renderer* renderer;
     SDL_Event touche;
+    SDL_Rect rectangle;
     int jeu = 0;
 
     SDL_Init(SDL_INIT_VIDEO);
     SDL_version nb;
     SDL_VERSION(&nb);
-
-    point.x = 0;
-    point.y = 0;
-    point.h = 30;
-    point.w = 10;
 
     pwindow=SDL_CreateWindow("test", SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
@@ -50,6 +47,17 @@ int main(int argc, char **argv) {
         1080,
         SDL_WINDOW_FULLSCREEN);// Cree la fenêtre de dimension 1920px par 1080px en fullscreen
 
+    renderer = SDL_CreateRenderer(pwindow, -1, SDL_RENDERER_SOFTWARE);
+
+    rectangle.x =20;
+    rectangle.y = 500;
+    rectangle.h = 480;
+    rectangle.w = 220;
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderDrawRect(renderer,&rectangle);
+    SDL_RenderPresent(renderer);
+
+    
     while (jeu==0) 
     {
         SDL_PollEvent(&touche);// Récupération des actions de l'utilisateur
@@ -58,9 +66,30 @@ int main(int argc, char **argv) {
             case SDL_KEYUP:
                 if (touche.key.keysym.sym == SDLK_ESCAPE)// Regarde si == touche ESC
                 {
+                    SDL_DestroyRenderer(renderer);
                     SDL_DestroyWindow(pwindow); // Detruit la fenetre
                     jeu = 1;
-                }break;
+                }
+                break;
+            case SDL_KEYDOWN:
+                if (touche.key.keysym.sym == SDLK_d)
+                {
+                    if (rectangle.x < 1680)
+                    {
+                        rectangle.x += 20;
+                    }
+                    SDL_RenderDrawRect(renderer, &rectangle);
+                    SDL_RenderPresent(renderer);
+                }
+                else if (touche.key.keysym.sym == SDLK_q)
+                {
+                    if (rectangle.x > 20)
+                    {
+                        rectangle.x -= 20;
+                    }
+                    SDL_RenderDrawRect(renderer, &rectangle);
+                    SDL_RenderPresent(renderer);
+                }
         }
     }
 
@@ -68,8 +97,7 @@ int main(int argc, char **argv) {
     
 
     printf("bienvenue sur la sdl %d.%d.%d !\n", nb.major, nb.minor, nb.patch);
-    gauche(&point);
-    printf("%d", point.x);
+
 
 
     SDL_Quit();
