f8a292871d229ed68e71c678a759d67cd05e11e3
matteobelin <matteobelin85@gmail.com>
2023-04-08 23:24:47 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,12 +3,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void affichage(SDL_Texture* texture, SDL_Renderer* renderer, SDL_Rect* rectangle)
+void affichage(SDL_Texture* texture, SDL_Renderer* renderer, SDL_Rect* rectangle, SDL_Rect* rectangleCoup,int *pcoup)
 {
     SDL_RenderClear(renderer);
     SDL_RenderCopy(renderer, texture, NULL, NULL);
     SDL_SetRenderDrawColor(renderer, 0, 255, 0, SDL_ALPHA_OPAQUE);
     SDL_RenderDrawRect(renderer, rectangle);
+    if (*pcoup != 0) {
+        SDL_RenderDrawRect(renderer, rectangleCoup);
+    }
     SDL_RenderPresent(renderer);
 }
 
@@ -18,11 +21,14 @@ int main(int argc, char** argv) {
     Uint8 const* touche;
     SDL_Event touche2;
     SDL_Rect rectangle = { 20, 500, 220, 500 };
+    SDL_Rect rectangleCoup = { 0, 0, 0, 100 };
     SDL_Surface* fond;
     SDL_Texture* texture;
     int jeu = 0;
     int saut = 0;
     int snick = 0;
+    int coup = 0;
+    int cote = 0;
 
     SDL_Init(SDL_INIT_VIDEO);
     SDL_version nb;
@@ -41,7 +47,6 @@ int main(int argc, char** argv) {
 
 
     SDL_RenderCopy(renderer, texture, NULL, NULL);
-    SDL_RenderPresent(renderer);// met a jour le 
     SDL_RenderPresent(renderer);// met a jour le renderer
 
 
@@ -51,7 +56,7 @@ int main(int argc, char** argv) {
         touche = SDL_GetKeyboardState(NULL);
 
 
-        if (touche[SDL_SCANCODE_D]) {
+        if (touche[SDL_SCANCODE_D] && (coup==0)) {
             if (rectangle.x < 1680)
             {
                 if (snick == 0) {
@@ -60,11 +65,14 @@ int main(int argc, char** argv) {
                 else {
                     rectangle.x += 1;
                 }
-                
+                cote = 1;
+            }
+            if (rectangle.x > 1500) {
+                cote = 0;
             }
         }
 
-        if (touche[SDL_SCANCODE_A] ) {
+        if (touche[SDL_SCANCODE_A] && (coup == 0)) {
 
             if (rectangle.x > 20)
             {
@@ -74,17 +82,20 @@ int main(int argc, char** argv) {
                 else {
                     rectangle.x -= 1;
                 }
-                
+                cote = 2;
+            }
+            if (rectangle.x < 200) {
+                cote = 0;
             }
 
         }
 
-        if ((touche[SDL_SCANCODE_S]) && (saut == 0)) {
+        if ((touche[SDL_SCANCODE_S]) && (saut == 0) && (coup == 0)) {
             rectangle.y = 750;
             rectangle.h = 250;
             snick = 1;
         }
-        if ((touche[SDL_SCANCODE_W]) && (saut == 0) && (snick == 0))
+        if ((touche[SDL_SCANCODE_W]) && (saut == 0) && (snick == 0) && (coup==0))
         {
             if (rectangle.y > 150) {
                 saut = 1;
@@ -112,12 +123,110 @@ int main(int argc, char** argv) {
         }
 
 
-        if ((touche[SDL_SCANCODE_S] == 0) && (snick == 1) && (saut == 0)) {
+        if ((touche[SDL_SCANCODE_S] == 0) && (snick == 1) && (saut == 0) && (coup == 0)) {
             rectangle.y = 500;
             rectangle.h = 500;
             snick = 0;
         }
-        affichage(texture, renderer, &rectangle);
+
+        if ((touche[SDL_SCANCODE_C]) && (saut==0)) {
+            if ((cote == 1) && (snick == 0) ) {
+                coup = 1;
+                cote = 3;// Pour pas qu'il repasse a coup 1
+            }
+            if ((cote == 2) && (snick == 0)) {
+                coup = 3;
+                cote = 3; // Pour pas qu'il repasse a coup 3
+            }
+
+            if ((cote == 1) && (snick == 1)) {
+                coup = 5;
+                cote = 3;
+            }
+
+            if ((cote == 2) && (snick == 1)) {
+                coup = 7;
+                cote = 3;
+            }
+        }
+        
+
+        if ((coup == 1) && (saut==0)) {
+            rectangleCoup.h = 100;
+            rectangleCoup.x = rectangle.x + rectangle.w;
+            rectangleCoup.y =rectangle.y+ 100;
+            if (rectangleCoup.w > 220) {
+                coup = 2;
+            }
+            rectangleCoup.w += 2;
+
+        }
+        if ((coup == 2) && (saut == 0)) {
+            rectangleCoup.w -= 2;
+            if (rectangleCoup.w == 0) {
+                coup = 0;
+                cote = 1;
+            }
+        }
+        if ((coup == 3) && (saut == 0)) {
+            rectangleCoup.h = 100;
+            rectangleCoup.x = rectangle.x;
+            rectangleCoup.y = rectangle.y + 100;
+            if (rectangleCoup.w < -220) {
+                coup = 4;
+            }
+            rectangleCoup.w -= 2;
+        }
+
+        if ((coup == 4) && (saut == 0)) {
+            rectangleCoup.w += 2;
+            if (rectangleCoup.w == 0) {
+                coup = 0;
+                cote = 2;
+            }
+        }
+
+        if ((coup == 5) && (saut == 0)) {
+            rectangleCoup.h = 100;
+            rectangleCoup.x = rectangle.x+rectangle.w;
+            rectangleCoup.y = rectangle.y+100;
+            if (rectangleCoup.w > 180) {
+                coup = 2;
+            }
+            rectangleCoup.w += 2;
+        }
+
+        if ((coup == 6) && (saut == 0)) {
+            rectangleCoup.w -= 2;
+            if (rectangleCoup.w == 0) {
+                coup = 0;
+                cote = 1;
+            }
+        }
+
+        if ((coup == 7) && (saut == 0)) {
+            rectangleCoup.h = 100;
+            rectangleCoup.x = rectangle.x;
+            rectangleCoup.y = rectangle.y + 100;
+            if (rectangleCoup.w < -180) {
+                coup = 8;
+            }
+            rectangleCoup.w -= 2;
+
+        }
+
+        if ((coup == 8) && (saut == 0)) {
+            rectangleCoup.w += 2;
+            if (rectangleCoup.w == 0) {
+                coup = 0;
+                cote = 2;
+            }
+
+        }
+
+
+
+        
 
 
         SDL_PollEvent(&touche2);// Récupération des actions de l'utilisateur
@@ -136,6 +245,7 @@ int main(int argc, char** argv) {
             }
             break;
         }
+        affichage(texture, renderer, &rectangle, &rectangleCoup,&coup);
     }
 
 
